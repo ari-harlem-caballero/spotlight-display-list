5366bfe72304f9674a97c89450ad2cdb9b2582fa
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-07 11:44:27 -0800

    created first array, nested

---
+++ array-one.js
@@ -0,0 +1,47 @@
+export bobsChar [
+    {
+        name: 'bob',
+        type: 'father',
+        job: 'restaurant owner',
+        bestFriend: {
+            name: 'louise',
+            favActivity: 'tormenting'
+        }   
+    },
+    {
+        name: 'linda',
+        type: 'mother',
+        job: 'entertainment',
+        bestFriend: {
+            name: 'gene',
+            favActivity: 'dress up'
+        }   
+    },
+    {
+        name: 'tina',
+        type: 'eldest child',
+        job: 'studying butts',
+        bestFriend: {
+            name: 'horses',
+            favActivity: 'wearing jorts'
+        }   
+    },
+    {
+        name: 'gene',
+        type: 'middle child',
+        job: 'musical expert',
+        bestFriend: {
+            name: 'linda',
+            favActivity: 'singing'
+        }   
+    },
+    {
+        name: 'louise',
+        type: 'youngest child',
+        job: 'world domination',
+        bestFriend: {
+            name: 'no one, but secretly bob',
+            favActivity: 'gardening'
+        }   
+    },
+];
