7497d5c77cb73b2868d9da5d8cc99274f6d0cc6c
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-07 11:59:01 -0800

    created elems for bobs render

---
+++ render-bobs.js
@@ -0,0 +1,11 @@
+export function renderBobs() {
+    const bobsDiv = document.createElement('div');
+    const bobsName = document.createElement('p');
+    const bobsType = document.createElement('p');
+    const bobsJob = document.createElement('p');
+
+    const bestieDiv = document.createElement('div');
+    const bestieName = document.createElement('p');
+    const bestieActivity = document.createElement('p');
+
+}
