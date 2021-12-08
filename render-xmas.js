7d78336e35afe541b736a5e13c640ff333461068
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-08 09:33:45 -0800

    added background img to xmasElem--tree, styled in css

--- render-xmas.js
+++ render-xmas.js
@@ -4,5 +4,9 @@ export function renderXmas(xmasData) {
     xmasElem.textContent = xmasData;
     xmasElem.classList.add('xmas-items');
 
+    if (xmasData === 'tree') {
+        xmasElem.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2020/12/19/15/32/socks-5844679_960_720.jpg')";
+    }
+
     return xmasElem;
 }
