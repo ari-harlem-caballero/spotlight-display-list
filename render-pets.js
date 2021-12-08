46b76f0d4c8a03b723f6800e4a7f21ae6351fc81
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-07 19:11:26 -0800

    added quotes

--- render-pets.js
+++ render-pets.js
@@ -6,14 +6,13 @@ export function renderPets(pet) {
     const petInfo = document.createElement('p');
 
     //add classlist to DIV -- classlist.add('ID')
-    petsDiv.classList.add('pet-list');
+    petsDiv.classList.add('my-pets');
     //add text to P -- textContent
     petName.textContent = `${pet.name}`;
     petRank.textContent = `ranked: ${pet.rank}`;
     petInfo.textContent = `is a ${pet.type}, ${pet.age} years old, with ${pet.color} fur`;
     //append P to DIV
-    petsDiv.append(petName,petRank, petInfo);
-    console.log(petsDiv);
+    petsDiv.append(petName, petRank, petInfo);
     //return DIV
     return petsDiv;
 }
---
+++ tv-data.js
@@ -0,0 +1,32 @@
+export const favTvShows = [
+    {
+        title: `Bob's Burgers`,
+        episodes: '226',
+        active: 'yes', 
+        quotes: [
+            `Linda: Ooh! And get all my dresses and wigs! Gene: But what'll you wear? --S8.Ep11`,
+            `Bob: I'm like a british lady in my garden. And my plants agree. Plants: Mm-hmm. --S5.Ep10`,
+            `Gene: I'm just a girl with a dream who got tired of hearing the word no! --S3.Ep20`
+        ]
+    },
+    {
+        title: 'The Office',
+        episodes: '188',
+        active: 'no',
+        quotes: [
+            `Dwight: In the wild, there is no healthcare. In the wild healthcare is ‘Ow, I hurt my leg. I can’t run. A lion eats me, and I’m dead.’ Well, I’m not dead. I’m the lion. You’re dead. --S1.Ep3`,
+            `Michael: I'm Beyonce always. --S6.Ep25`,
+            `Michael: I'm not superstitious, but I am a little stitious. --S4.Ep1`
+        ]
+    },
+    {
+        title: 'What We Do in the Shadows',
+        episodes: '31',
+        active: 'yes',
+        quotes: [
+            `Colin: One of the best ways to drain people's energy nowadays is via the internet. --S2.Ep5`,
+            `Nandor: I would like us to get colorful dust that sparkles. Guillermo: Glitter? Nandor: Yes, get me some glitter. Guillermo: Whatever for, Master? Nandor: Well, I want to do something special for the immortal one's arrival. I am going to sprinkle it on my face and on my body. Like Twilight. --S1.Ep1`,
+            `Jeff: This is where I work...hence, the, why I'm working here. Nadja: Amazing, you're protecting a vast empire. Jeff: All 3 floors. --S1.Ep1`
+        ]
+    }
+];
