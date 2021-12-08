9c80ae08b4f8bc1c298416e6e51fc28d93a26e94
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-08 10:24:32 -0800

    added color to pet rank

--- render-pets.js
+++ render-pets.js
@@ -15,10 +15,28 @@ export function renderPets(pet) {
     petInfo.classList.add('info');
     petType.classList.add('type');
 
+    
+    
     //add text to P -- textContent
     petName.textContent = `${pet.name}`;
     petType.textContent = `is a ${pet.type}, ${pet.age} years old, with ${pet.color} fur`;
     petRank.textContent = `ranked: ${pet.rank}`;
+    
+    if (petRank.textContent === `ranked: 1`) {
+        petRank.style.backgroundColor = 'green'; 
+    }
+    if (petRank.textContent === `ranked: 2`) {
+        petRank.style.backgroundColor = 'yellowgreen'; 
+    }
+    if (petRank.textContent === `ranked: 3`) {
+        petRank.style.backgroundColor = 'orange'; 
+    }
+    if (petRank.textContent === `ranked: 4`) {
+        petRank.style.backgroundColor = 'chocolate'; 
+    }
+    if (petRank.textContent === `ranked: 5`) {
+        petRank.style.backgroundColor = 'red'; 
+    }
     //append P to DIV
     petsDiv.append(petName, petInfo);
 
