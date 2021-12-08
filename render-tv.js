41aa49c96ab7ea51cb6084a94c592a8adbe13eaa
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-08 08:50:57 -0800

    styled

--- render-tv.js
+++ render-tv.js
@@ -2,19 +2,19 @@ export function renderTvShows(tvShow) {
 
     const tvDiv = document.createElement('div');
     const tvTitle = document.createElement('p');
-
+    
     tvDiv.classList.add('tv-show');
-
+    
     tvTitle.textContent = `${tvShow.title}: ${tvShow.episodes}, ${tvShow.active}`;
-
+    
     tvDiv.append(tvTitle);
-
+    
     for (let shows of tvShow.quotes) {
         const showEl = document.createElement('p');
-
+        
         showEl.textContent = shows;
-
-        showEl.classList.add('quotes');
+        
+        showEl.classList.add('single-quotes');
 
         tvDiv.append(showEl);
     }
--- xmas-data.js
+++ xmas-data.js
@@ -2,6 +2,6 @@ export const xmasThings = [
     'tree',
     'lights',
     'decorations',
-    'movies',
+    'stories',
     'socks'
 ];
