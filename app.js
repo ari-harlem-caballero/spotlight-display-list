01a080a3550112e056083f464a5b94d5a7a92ac4
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-07 19:53:19 -0800

    created render-tv.js, looped tvshows in app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // step 0: import array
 import { renderBobs } from './render-bobs.js';
 import { renderPets } from './render-pets.js';
+import { renderTvShows } from './render-tv.js';
 import { bobsChar } from './bobs-data.js';
 import { myPets } from './pets-data.js';
+import { favTvShows } from './tv-data.js';
+
 // step 1: grab a div
 const bobsDiv = document.getElementById('bobs-list');
 const petsDiv = document.getElementById('pet-list');
+const tvDiv = document.getElementById('tv-show-list');
 // step 2: loop through array
 
 for (let bob of bobsChar) {
@@ -19,4 +23,10 @@ for (let pet of myPets) {
     const petList = renderPets(pet);
 
     petsDiv.append(petList);
+}
+
+for (let tvShow of favTvShows) {
+    const tvList = renderTvShows(tvShow);
+
+    tvDiv.append(tvList);
 }
